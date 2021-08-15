8bed885bb74ab54d17f90c4a06d6f3b54375e6b5
BishawjeetPaul <bishawjeet.8809@gmail.com>
2021-08-15 13:55:51 +0530

    Initial Commit

--- restFramework_API/settings.py
+++ restFramework_API/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v=sz5p6+=mqqw7$jm)hg=+v*kz6z6o=x3t1xx!i1d0q%5s*&48'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["restapiframework33.herokuapp.com"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+
+import dj_database_url
+prod_db=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
